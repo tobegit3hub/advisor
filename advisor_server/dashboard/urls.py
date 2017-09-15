deb053748ab045243e86a85ee310275ece8bd57f
tobe <tobeg3oogle@gmail.com>
2017-09-15 23:53:12 +0800

    Support list and create in dashboard

--- advisor_server/dashboard/urls.py
+++ advisor_server/dashboard/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^studies$', views.studies, name='studies'),
-    url(r'^studies/(?P<study_id>[\w.-]+)$', views.study, name='study'),
-    url(r'^studies/(?P<study_id>[\w.-]+)/trials$', views.trials,
-        name='trials'),
-    url(r'^studies/(?P<study_id>[\w.-]+)/trials/(?P<trial_id>[\w.-]+)$',
-        views.trial,
-        name='trial'),
+    url(r'^v1/studies$', views.v1_studies, name='v1_studies'),
+    url(r'^v1/studies/(?P<study_id>[\w.-]+)$', views.v1_study,
+        name='v1_study'),
+    url(r'^v1/trials$', views.v1_trials, name='v1_trials'),
+    url(r'^v1/trials/(?P<trial_id>[\w.-]+)$', views.v1_trial, name='v1_trial'),
 ]
--- advisor_server/dashboard/views.py
+++ advisor_server/dashboard/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+import requests
+
+from django.contrib import messages
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -14,27 +21,65 @@ from django.conf import settings
 from django import forms
 from django.contrib.admin.views.decorators import staff_member_required
 
+from suggestion.models import Study
+from suggestion.models import Trial
+
 
 def index(request):
+  try:
+    studies = [study.to_json() for study in Study.objects.all()]
+  except Study.DoesNotExist:
+    studies = []
+
+  try:
+    trials = [trial.to_json() for trial in Trial.objects.all()]
+  except Study.DoesNotExist:
+    trials = []
+
+  context = {"success": True, "studies": studies, "trials": trials}
+  return render(request, "index.html", context)
+
+
+@csrf_exempt
+def v1_studies(request):
+  if request.method == "POST":
+    name = request.POST.get("name", "")
+    study_configuration = request.POST.get("study_configuration", "")
+
+    data = {"name": name, "study_configuration": study_configuration}
+
+    url = "http://127.0.0.1:8000/suggestion/v1/studies"
+    response = requests.post(url, json=data)
+    messages.info(request, response.content)
+    return redirect("index")
+  else:
+    return JsonResponse({"error": "Unsupported http method"})
+
+
+@csrf_exempt
+def v1_study(request):
   context = {}
   return render(request, "index.html", context)
 
 
-def studies(request):
-  context = {}
-  return render(request, "index.html", context)
+@csrf_exempt
+def v1_trials(request):
+  if request.method == "POST":
+    study_id = request.POST.get("study_id", "")
+    name = request.POST.get("name", "")
+
+    data = {"name": name}
+
+    url = "http://127.0.0.1:8000/suggestion/v1/studies/{}/trials".format(
+        study_id)
+    response = requests.post(url, json=data)
+    messages.info(request, response.content)
+    return redirect("index")
+  else:
+    return JsonResponse({"error": "Unsupported http method"})
 
 
-def study(request, study_id):
-  context = {}
-  return render(request, "index.html", context)
-
-
-def trials(request, study_id):
-  context = {}
-  return render(request, "index.html", context)
-
-
-def trial(request, study_id, trial_id):
+@csrf_exempt
+def v1_trial(request, study_id, trial_id):
   context = {}
   return render(request, "index.html", context)
