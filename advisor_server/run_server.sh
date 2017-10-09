86150c7f690406d2f885b9c43d2065d66ed193db
tobe <tobeg3oogle@gmail.com>
2017-10-09 15:54:11 +0800

    Support jupyter notebook in index page

--- advisor_server/run_server.sh
+++ advisor_server/run_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./manage.py runserver 0.0.0.0:8000
+
+# ipython notebook --config ./jupyter_notebook_config.py
---
+++ jupyter_notebook_config.py
@@ -0,0 +1,22 @@
+import os
+from IPython.lib import passwd
+
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+c.MultiKernelManager.default_kernel_name = 'python2'
+
+# sets a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+  c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+  del os.environ['PASSWORD']
+else:
+  c.NotebookApp.password = passwd('advisor')
+
+# Support iframe
+#c.NotebookApp.tornado_settings = {'headers': {'X-Frame-Options': 'ALLOW-FROM http://127.0.0.1:8000'}}
+c.NotebookApp.tornado_settings = {
+    "headers": {
+        "Content-Security-Policy": "frame-ancestors self http://localhost:8000; report-uri /api/security/csp-report"
+    }
+}
