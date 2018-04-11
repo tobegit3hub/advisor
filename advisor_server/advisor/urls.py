26cfd133e7476e543f571085d08a8087a093f40d
tobe <tobeg3oogle@gmail.com>
2018-04-11 18:07:30 +0800

    Remove notebook page and redirect index to dashboard by default

--- advisor_server/advisor/urls.py
+++ advisor_server/advisor/urls.py
@@ -21,7 +21,10 @@ from django.contrib.auth import views as auth_views
 from dashboard import views as dashboard_views
 
 urlpatterns = [
-    url(r'^$', dashboard_views.home, name='home'),
+    # Redirect to dashboard instead of login page
+    # url(r'^$', dashboard_views.home, name='home'),
+    url(r'^$', include('dashboard.urls')),
+
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
--- advisor_server/run_server.sh
+++ advisor_server/run_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ./manage.py runserver 0.0.0.0:8000
+# nohup ./manage.py runserver 0.0.0.0:80 &
 
 # ipython notebook --config ./jupyter_notebook_config.py
+# nohup ipython notebook --config ./jupyter_notebook_config.py --allow-root &
