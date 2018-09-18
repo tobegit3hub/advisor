af51ee1162b9c33a7878dc73e32ac99f56cf8acf
tobe <tobeg3oogle@gmail.com>
2018-09-18 21:32:11 +0800

    Implement features for tpe algorithm to use trained trials

--- advisor_server/suggestion/algorithm/tpe_test.py
+++ advisor_server/suggestion/algorithm/tpe_test.py
@@ -58,25 +58,55 @@ class TpeAlgorithmTest(TestCase):
   def test_get_new_suggestions(self):
     tpeAlgorithm = TpeAlgorithm()
 
-    new_trials = tpeAlgorithm.get_new_suggestions(
-        self.study.id, number=1)
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, number=1)
     new_trial = new_trials[0]
     new_parameter_values_json = json.loads(new_trial.parameter_values)
 
-    self.assertTrue(0.99 >= new_parameter_values_json["l1_normalization"] >= 0.01)
+    self.assertTrue(
+        0.99 >= new_parameter_values_json["l1_normalization"] >= 0.01)
     self.assertTrue(0.5 >= new_parameter_values_json["learning_rate"] >= 0.01)
     self.assertTrue(new_parameter_values_json["hidden2"] in [8, 16, 32, 64])
-    self.assertTrue(new_parameter_values_json["optimizer"] in ["sgd", "adagrad", "adam", "ftrl"])
+    self.assertTrue(new_parameter_values_json["optimizer"] in [
+        "sgd", "adagrad", "adam", "ftrl"
+    ])
 
   def test_get_multiple_new_suggestions(self):
     tpeAlgorithm = TpeAlgorithm()
 
     # Assert getting one trial
-    new_trials = tpeAlgorithm.get_new_suggestions(
-        self.study.id, number=1)
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, number=1)
     self.assertEqual(len(new_trials), 1)
 
     # Assert getting multiple trials
-    new_trials = tpeAlgorithm.get_new_suggestions(
-        self.study.id, number=10)
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, number=10)
     self.assertEqual(len(new_trials), 10)
+
+  def test_complete_and_get_new_suggestions(self):
+    tpeAlgorithm = TpeAlgorithm()
+
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, [], 1)
+    new_trials[0].status = "Completed"
+    new_trials[0].objective_value = 0.6
+    new_trials[0].save()
+
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, [], 1)
+    new_trials[0].status = "Completed"
+    new_trials[0].objective_value = 0.7
+    new_trials[0].save()
+
+    new_trials = tpeAlgorithm.get_new_suggestions(self.study.id, [], 3)
+
+    # Assert getting two trials
+    self.assertEqual(len(new_trials), 3)
+
+    # Assert getting the trials
+    new_trial = new_trials[0]
+    new_parameter_values = new_trial.parameter_values
+    new_parameter_values_json = json.loads(new_parameter_values)
+    self.assertTrue(
+        0.99 >= new_parameter_values_json["l1_normalization"] >= 0.01)
+    self.assertTrue(0.5 >= new_parameter_values_json["learning_rate"] >= 0.01)
+    self.assertTrue(new_parameter_values_json["hidden2"] in [8, 16, 32, 64])
+    self.assertTrue(new_parameter_values_json["optimizer"] in [
+        "sgd", "adagrad", "adam", "ftrl"
+    ])
