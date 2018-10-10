5bd963d729c36f02d0d1a0d2a8477ab55180ce80
tobe <tobeg3oogle@gmail.com>
2018-10-10 20:39:17 +0800

    Support markdown

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# Advisor Docs
+
+## Installation
+
+```
+pip install sphinx sphinx-autobuild sphinx_rtd_theme recommonmark
+```
+
+## Deployment
+
+Change config refer to https://www.xncoding.com/2017/01/22/fullstack/readthedoc.html .
+
+```
+make html
+```
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import recommonmark
+from recommonmark.transform import AutoStructify
+
 
 # -- Project information -----------------------------------------------------
 
@@ -50,7 +53,12 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+#source_suffix = '.rst'
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -179,3 +187,10 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
