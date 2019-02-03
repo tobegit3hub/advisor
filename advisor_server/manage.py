10f945f2472c22def3be73d2874468e4c422aa34
areeh <arehaartveit@gmail.com>
2019-02-03 13:04:45 +0100

    set django ver in manage.py, add travis

--- advisor_server/manage.py
+++ advisor_server/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+  os.environ.setdefault("DJANGO_VERSION", "2.1.5")
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advisor.settings")
   try:
     from django.core.management import execute_from_command_line
